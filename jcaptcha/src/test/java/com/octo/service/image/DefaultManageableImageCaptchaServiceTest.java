38a8ebc0306c2d2e838ae51990c1689db736e3b6
Alberto Fernandez <infjaf@gmail.com>
2024-06-13 20:51:56 +0200

    add test for DefaultManageableImageCaptchaService

---
+++ jcaptcha/src/test/java/com/octo/service/image/DefaultManageableImageCaptchaServiceTest.java
@@ -0,0 +1,31 @@
+package com.octo.service.image;
+
+import java.awt.image.BufferedImage;
+import java.util.Locale;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.octo.captcha.service.image.DefaultManageableImageCaptchaService;
+import com.octo.captcha.service.image.ImageCaptchaService;
+
+public class DefaultManageableImageCaptchaServiceTest {
+	
+	public DefaultManageableImageCaptchaServiceTest() {
+		super();
+	}
+	
+	// DefaultManageableImageCaptchaService is the service used by SimpleImageCaptchaServlet
+	// The first and default user of this library
+	// Check if it's working
+	
+	@Test
+	public void basicTest() {
+		ImageCaptchaService service = new DefaultManageableImageCaptchaService();
+		BufferedImage challenge = service.getImageChallengeForID("testId", Locale.US);
+		Assertions.assertNotNull(challenge);
+		Assertions.assertTrue(challenge.getWidth() > 10);
+		Assertions.assertTrue(challenge.getHeight() > 10);
+	}
+
+}
